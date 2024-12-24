34021c92eea139ee38ea4868934365f9eb2c4b00
Nikola <nikola@example.com>
2024-12-24 15:25:52 +0100

    routing works

--- reactvite/src/App.tsx
+++ reactvite/src/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
 import './App.css'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./components/Login";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
@@ -13,7 +12,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import Logout from "./components/Logout";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -21,25 +19,15 @@ function App() {
         <AuthProvider>
           <Router>
             <NavBar />
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+            </Routes>
           </Router>
         </AuthProvider>
       </div >
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <ToastContainer />
     </>
   )
