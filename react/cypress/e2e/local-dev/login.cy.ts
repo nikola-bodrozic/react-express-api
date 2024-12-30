5e84f896736803e0466ef687ff3111f919dac210
Nikola <nikola@example.com>
2024-12-30 23:19:38 +0100

    intercepting POST call & setting mock values

--- react/cypress/e2e/local-dev/login.cy.ts
+++ react/cypress/e2e/local-dev/login.cy.ts
@@ -1,31 +1,17 @@
 /// <reference types="cypress" />
 describe("Login Test", () => {
+  const apiBaseURL = "http://localhost:4000/api/v1";
+  const reactBaseURL = "http://localhost:5173/";
   beforeEach(() => {
-    cy.visit("http://localhost:5173/");
+    cy.visit(reactBaseURL);
     cy.get("#about").click();
     cy.url().should("include", "/about");
     cy.get("#dashboard").click();
     cy.url().should("include", "/login");
   });
 
-  it("should get error message when entering invalid credentials", () => {
-    cy.intercept("POST", "http://localhost:4000/api/v1/login").as("postData");
-
-    cy.get('input[name="username"]').type("u1");
-    cy.get('input[name="password"]').type("p1");
-    cy.get('button[type="submit"]').click();
-    cy.get("#loader").should("be.visible");
-    cy.wait("@postData").then((interception) => {
-      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-      interception.response &&
-        expect(interception.response.statusCode).to.equal(422);
-      cy.get("#loader").should("not.exist");
-    });
-    cy.get("#error").children().should("have.length.greaterThan", 0);
-  });
-
   it("should log in with valid credentials", () => {
-    cy.intercept("POST", "http://localhost:4000/api/v1/login").as("postData");
+    cy.intercept("POST", `${apiBaseURL}/login`).as("postData");
 
     cy.get('input[name="username"]').type("username1");
     cy.get('input[name="password"]').type("pass1");
@@ -39,5 +25,39 @@ describe("Login Test", () => {
     });
     cy.url().should("include", "/dashboard");
     cy.get("#msg").should("contain", "welcome to dasboard");
+  });
+
+  it("should get error message when entering invalid credentials", () => {
+    cy.intercept("POST", `${apiBaseURL}/login`, {
+      statusCode: 422,
+      body: {
+        errors: [
+          {
+            msg: "test Username must be at least 5 characters long",
+          },
+          {
+            msg: "test Password must be at least 5 characters long",
+          },
+        ],
+      },
+    }).as("postData");
+
+    cy.get('input[name="username"]').type("u1");
+    cy.get('input[name="password"]').type("p1");
+    cy.get('button[type="submit"]').click();
+    cy.get("#loader").should("be.visible");
+    cy.wait("@postData").then((interception) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      interception.response &&
+        expect(interception.response.statusCode).to.equal(422);
+      cy.get("#loader").should("not.exist");
+    });
+    cy.get("#error").children().should("have.length.greaterThan", 0);
+    cy.get("#error .err-item")
+      .first()
+      .should("have.text", "test Username must be at least 5 characters long");
+    cy.get("#error .err-item")
+      .eq(1)
+      .should("have.text", "test Password must be at least 5 characters long");
   });
 });
--- react/src/components/Login/Login.tsx
+++ react/src/components/Login/Login.tsx
@@ -1,77 +1,100 @@
-// Login.tsx
-import React, { useState } from 'react';
-import "./Login.css"
-import { useAuth } from '../../AuthContext';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Login.css";
+import { useAuth } from "../../AuthContext";
+import { useNavigate } from "react-router-dom";
 import { axiosClient } from "../../axiosClient";
-import { AxiosResponse } from 'axios';
-import { toast } from 'react-toastify';
-import Loader from 'react-js-loader';
+import { AxiosResponse } from "axios";
+import { toast } from "react-toastify";
+import Loader from "react-js-loader";
 
 interface errorMsg {
-    type?: string;
-    value?: string;
-    msg?: string;
+  type?: string;
+  value?: string;
+  msg?: string;
 }
 
 const Login = () => {
-    const { login, logout, renderName } = useAuth();
-    const navigate = useNavigate();
+  const { login, logout, renderName } = useAuth();
+  const navigate = useNavigate();
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState<string[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleLogin = async (e: React.FormEvent) => {
-        e.preventDefault()
-        setIsLoading(true)
-        try {
-            const res: AxiosResponse = await axiosClient.post("/login", { username, password });
-            renderName(res.data.user.name)
-            login()
-            toast.success('Logged in successfully!');
-            setIsLoading(false)
-            navigate('/dashboard')
-        } catch (error: any) {
-            setIsLoading(false)
-            const tmpErr: string[] = [];
-            error.response.data.errors.map((el: errorMsg) => tmpErr.push(el.msg as string))
-            setErrorMessage(tmpErr);
-            logout()
-        }
-    };
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const res: AxiosResponse = await axiosClient.post("/login", {
+        username,
+        password,
+      });
+      renderName(res.data.user.name);
+      login();
+      toast.success("Logged in successfully!");
+      setIsLoading(false);
+      navigate("/dashboard");
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      setIsLoading(false);
+      const tmpErr: string[] = [];
+      error.response.data.errors.map((el: errorMsg) =>
+        tmpErr.push(el.msg as string)
+      );
+      setErrorMessage(tmpErr);
+      logout();
+    }
+  };
 
-    return (isLoading?<div className='loaderHolder' id="loader"><Loader type="spinner-circle" bgColor={"green"} color={"red"} size={150} /></div>:
+  return isLoading ? (
+    <div className="loaderHolder" id="loader">
+      <Loader
+        type="spinner-circle"
+        bgColor={"green"}
+        color={"red"}
+        size={150}
+      />
+    </div>
+  ) : (
+    <>
+      <div id="error">
+        {errorMessage.length > 0 &&
+          errorMessage.map((el: string, index: number) => (
+            <div key={index} className="err-item">
+              {el}
+            </div>
+          ))}
+      </div>
+
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
         <div>
-            <div id="error"> {errorMessage.length > 0 && errorMessage.map((el: string, index: number) => <div key={index} style={{ color: "red" }}>{el}</div>)} </div>
-            
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label htmlFor="username">Username: </label>
-                    <input
-                        type="text"
-                        name="username"
-                        value={username}
-                        placeholder="Username"
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password: </label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={password}
-                        placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button type="submit" className='button' name="login">Login</button>
-            </form>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
-    );
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="button" name="login">
+          Login
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
